37710b55921e3b08b9c2f36c4c207a2578c0f701
MayankPant <mpant4512@gmail.com>
2024-11-11 23:59:33 +0530

    fix: Improved the look of entries.

--- src/Components/LogEntry.tsx
+++ src/Components/LogEntry.tsx
@@ -1,5 +1,5 @@
 import LanguageIcon from '@mui/icons-material/Language';
-import '../styles/LogEntry.css'
+import '../styles/LogEntry.css';
 import { useTheme } from "@mui/material";
 
 const LogEntry = (props: LogEntry) => {
@@ -9,20 +9,26 @@ const LogEntry = (props: LogEntry) => {
         color: theme.palette.text.secondary,
         border: `1px solid ${theme.palette.secondary.main}`,
         padding: '10px',
-    }
-    return(
+    };
+
+    return (
         <div style={styles} className="log-entry">
-            <LanguageIcon sx={{margin: '20px 10px 0px 0px', color: theme.palette.text.primary}} />
+            <LanguageIcon 
+                sx={{color: theme.palette.text.primary }} 
+                className="language-icon"
+            />
             <div className="entry-details">
-                <span style={{fontWeight: 900, fontSize: '1em', color: theme.palette.text.primary}}>{props.transactionType}</span>
-                <span>1.5 sec</span>
-                <span>{props.message}</span>
+                <span style={{ fontWeight: 900, fontSize: '1em', color: theme.palette.text.primary }}>
+                    {props.transactionType}
+                </span>
+                <span className="timing">1.5 sec</span>n
+                <span id="logMessage">{props.message}</span>
             </div>
             <div className="transaction-time">
                 {props.transactionTime}
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default LogEntry;
